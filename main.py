2ca20b01abef0039a8bba1dcab7d3d1cdc6e4aaa
irbispro10 <irbispro10@gmail.com>
2022-04-20 00:40:45 +0300

    2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import math
+
 import numpy as np
+import matplotlib.pyplot as plt
+# plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+# plt.show()
 def P_z_xi(Z, Po, Pl, index, k):
     p_z_xi = np.power(Po, Z[k][index])*np.power((1-Po), (1-Z[k][index]))
     for i in range(len(Z[k])):
@@ -6,18 +11,36 @@ def P_z_xi(Z, Po, Pl, index, k):
             p_z_xi*=(np.power(Pl, Z[k][i])*np.power((1-Pl), (1-Z[k][i])))
     return p_z_xi
 
-def Pt(P0, N, _lambda, t):
+def Pt(P0, N, _lambda, t): #априорная вероятность
     return (P0-1/N)*np.exp((-1*_lambda*N*t)/(N-1))+1/N
 
-def P_x_z(Z, index, P0, _lambda, t, Po, Pl, period):
-    tmp = 0
-    for i in len(Z[t/period]):
-        tmp+=(Pt(P0, len(Z[0]), _lambda, t) * P_z_xi(Z, Po, Pl, index, t / period))
-    res = (Pt(P0, len(Z[0]), _lambda, t)*P_z_xi(Z, Po, Pl, i, t/period))/tmp
-    return res
+def P_i(P_ob, P_lt, Z, index): #условная вероятность
+    tmp=np.power(P_ob, Z[index])*np.power((1-P_ob), (1-Z[index]))
+    print(tmp)
+    for i,z in enumerate(Z):
+        if i != index:
+            tmp*=np.power(P_lt, Z[i])*np.power((1-P_lt), (1-Z[i]))
+    print(tmp)
+
+
+period = 20
+P_ob=0.9
+P_lt=0.05
 
 
 
-Z = [[0,0,0,1],[0,1,0,0],[1,0,0,0]]
+Z = [[0,1,0,0],[0,1,0,0],[1,0,0,0]]
 
+P_i(P_ob, P_lt, Z[0], 0)
+
+X = []
+Y = []
+
+for x in range(0,60):
+    X.append(x)
+    Y.append(P_i(P_ob, P_lt, Z[math.floor(x/period)], 0))
+plt.plot(np.array(X), np.array(Y), ':b')
+# plt.plot(x, P_i(P_ob, P_lt, Z[np.round(float(x/period))], 0))
+# plt.plot(x, Pt(0.9, 5, 0.5, x), ':b')
+plt.show()
 print(P_z_xi(Z, 0.9, 0.1, 0,1))
